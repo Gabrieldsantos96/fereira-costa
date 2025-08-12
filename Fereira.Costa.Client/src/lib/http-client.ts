5792d2233f7441ba8ab26718866d4022675ed617
gabriel santos <gabrieltemp.mobile@gmail.com>
2025-08-12 01:05:54 -0300

    .

--- Fereira.Costa.Client/src/lib/http-client.ts
+++ Fereira.Costa.Client/src/lib/http-client.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 
+// @ts-expect-error: Property 'env' does not exist on type 'ImportMeta'
 const API_HTTP_URL = import.meta.env.VITE_API_URL;
 
 const httpClient = axios.create({
